62123d57302dfa2cf557e6c66b6a6d58c373b6e1
Karapet <suvaryankarapet1@gmail.com>
2022-02-11 11:48:10 +0400

    Show list in JSX / map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import './App.css';
-import {useState} from "react";
+import React from "react";
 
 function App() {
-  const [value, setValue] = useState("");
+
+    // const el = React.createElement('div', {}, 'ok ok');
+    // const el2 = React.createElement('div', {}, 'bye bye');
+    // const el3 = React.createElement('div', {}, 'yay');
+    //
+    // return React.createElement('div', {}, [el, el2, el3]); // third parameter can be array also in JSX
+    // in JSX child of element can be either element or array of element
+    // every element in the list of JSX should have unique key
+
+    // const el = <div>hello</div>;
+    // const el2 = <div>bye</div>;
+    // const arr = [el, el2];
+    const friends = [
+        {id: 1, firsName: 'Joe', lastName: "Jameson", age: 44},
+        {id: 2, firsName: 'Marry', lastName: "Green", age: 33},
+        {id: 3, firsName: 'Bill', lastName: "Jackson", age: 12},
+        {id: 4, firsName: 'Irina', lastName: "Jameson", age: 39},
+    ];
+    // const elements = friends.map(friend => {
+    //     return (
+    //         <div>
+    //             <h3>{friend.firsName}{friend.lastName}</h3>
+    //             <p>{friend.age}</p>
+    //         </div>
+    //     );
+    // })
   return (
-    <div className="App">
-        <h1>{value}</h1>
-      <input type="input" value={value} onChange={(e) => {
-          setValue(e.target.value);
-        }
-      }/>
-    </div>
+    // <div className="App">
+    //     {el}
+    //     {[el, el2]}
+    //     {arr}
+    // </div>
+      <div>
+          {friends.map(friend => {
+              return (
+                  <div key={friend.id}>
+                      <h3>{friend.firsName}{friend.lastName}</h3>
+                      <p>{friend.age}</p>
+                  </div>
+              );
+          })}
+      </div>
   );
 }
 
