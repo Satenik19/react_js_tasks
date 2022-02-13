007ac27f5743e65a74d36280e35219ee0463e381
Satenik <satenik.matevosyan@gmail.com>
2022-02-13 14:45:54 +0400

    Redux / create store / useDispatch / useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import React, {useContext, useEffect, useState} from 'react';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
-import TodoFooter from './TodoFooter';
-import Example from "./Example";
+import {useDispatch, useSelector} from "react-redux";
 
-// UseEffect with second parameter as empty array calls after mount
-// UseEffect with second parameter as not empty array calls each time when the value in array changes
-// UseEffect for mounted returns a function which calls before unmount
-// UseEffect for update returns a function which calls each time before calling next function in UseEffect
 function App() {
-    const [isVisible, setIsVisible] = useState(true);
+
+    const name = useSelector((state) => {
+        return state.currentUser.name;
+    });
+
+    const dispatch = useDispatch();
     return (
         <div className="App">
-            Hello Armenia
-            <button onClick={() => setIsVisible(!isVisible)}>click me</button>
-            {
-                isVisible ? <Example /> : null
-            }
+            <h1>{name}</h1>
+            <input type="text" value={name} onChange={(e) => {
+                dispatch({
+                    type: 'edit-current-user-name',
+                    payload: {
+                        name: e.target.value
+                    }
+                })
+            }}/>
         </div>
     );
 }
