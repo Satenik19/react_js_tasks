0af63b2f1785f71898fdfcb7a12bdef4c8ce1635
Satenik <satenik.matevosyan@gmail.com>
2022-02-13 15:34:43 +0400

    Middlewares in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,30 @@
-import { combineReducers } from 'redux';
-const { createStore } = require('redux');
 import { currentUserReducer, initialCurrentUser } from '../features/currentUser/currentUserSlice';
 import { initialTodos, todosReducer } from '../features/todos/todosSlice';
+import { applyMiddleware, combineReducers } from 'redux';
+const { createStore } = require('redux');
 
+function logger1(store) {
+    return function(next) {
+        return function(action) {
+            // setTimeout(() => {
+            //     next(action);
+            // }, 2000);
+            next(action);
+        }
+    }
+}
 
+const logger2 = (store) => (next) => (action) => {
+    return next(action);
+}
 const store = createStore(combineReducers({
     todos: todosReducer,
     currentUser: currentUserReducer
 }), {
     currentUser: initialCurrentUser,
-    friends: [],
-    posts: [],
+    // friends: [],
+    // posts: [],
     todos: initialTodos
-});
+}, applyMiddleware(logger1, logger2));
 
 export default store;
